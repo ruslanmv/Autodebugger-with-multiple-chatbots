897e4d8fe5c70f77ee871346bb93756a6e3c3b9a
Ruslan Magana Vsevolodovna <ruslan_mv@mail.ru>
2023-12-25 21:17:10 +0100

    Adding max attempt

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import subprocess
 import pandas as pd
 from utils import *
 import base64
+
 def run_code(code):
     try:
         output = subprocess.check_output(["python", "-c", code])
@@ -11,10 +12,15 @@ def run_code(code):
         return False, e.output.decode("utf-8")
 
 st.title("Auto Error Debugger Assistant with WatsonX")
+
 code_input = st.text_area("Paste your code here:", height=200)
 fixed_code = st.empty()
 output_zone = st.empty()
+
+max_attempts = st.slider("Select maximum number of attempts", 1, 10, 3)
+
 run_option = st.selectbox("Local Run Code:", ("Yes", "No"))
+
 log_data = []
 
 if st.button("Debug and Run"):
@@ -22,15 +28,15 @@ if st.button("Debug and Run"):
         code = code_input
         attempt = 1
         success = False
-        while success == False and attempt < 3:
+        while success == False and attempt <= max_attempts:
             output_zone.write(f"Attempt {attempt}: Running code...")
             success, output = run_code(code)
             if success:
-                output_zone.write(f"Code executed successfully.\n Output: {output}")
+                output_zone.write(f"Code executed successfully.\nOutput: {output}")
                 fixed_code.write("Suggested code:")
+                st.markdown("### Code suggested")
                 st.code(code, language='python')
-                log_data.append([attempt,code_input, code, "",success])
-                
+                log_data.append([attempt, code_input, code, "", success])
                 break
             else:
                 error = output
@@ -38,8 +44,8 @@ if st.button("Debug and Run"):
                 code = suggestion  # Update the code with the fixed version from the chatbot
                 output_zone.write("Trying again with the fixed code...")
                 success, output = run_code(code)
-                output_zone.write(f"Code executed successfully.\n Output: {output}")
-                log_data.append([attempt, code_input, code, error,success])
+                output_zone.write(f"Code executed successfully.\nOutput: {output}")
+                log_data.append([attempt, code_input, code, error, success])
                 attempt += 1
     else:
         output_zone.write("Code execution skipped.")
@@ -48,13 +54,13 @@ if st.button("Debug and Run"):
         suggestion = get_chatbot_suggestion(error, code)  # Use your chatbot API to get the suggestion
         code = suggestion  # Update the code with the fixed version from the chatbot
         fixed_code.write("Suggested fix applied:")
+        st.markdown("### Code suggested")
         st.code(code, language='python')
-        attempt=1
-        log_data.append([ attempt,code_input, code, "",""])
+        attempt = 1
+        log_data.append([attempt, code_input, code, "", ""])
 
-log_df = pd.DataFrame(log_data, columns=["Attempt","Initial Code",  "Suggested Code", "Error","Success Test"])
+log_df = pd.DataFrame(log_data, columns=["Attempt", "Initial Code", "Suggested Code", "Error", "Success Test"])
 log_df = log_df.reset_index(drop=True)
-
 
 if len(log_data) > 0:
     st.markdown("---")
